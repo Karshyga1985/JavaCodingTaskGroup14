d49c8e3a2950475efec11079e1f98cc38ed709e5
nazikt <nazik.turdakunova@gmail.com>
2024-01-31 09:22:13 -0500

    added solution for task 2

--- src/first_week_tasks/NazikFirstWeekTasks.java
+++ src/first_week_tasks/NazikFirstWeekTasks.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class NazikFirstWeekTasks {
 
-    public static void main(String[]args){
+    public static void main(String[] args) {
 
-        Scanner input = new Scanner (System.in);
+        Scanner input = new Scanner(System.in);
 
         System.out.println("Enter a number: ");
 
@@ -23,12 +23,28 @@ public class NazikFirstWeekTasks {
 
     }
 
-}
 /*
+Task1:
 Write a method which can identify given numbers is even or odd:
       identify(5) -> "Odd";
       identify(6) -> "Even"
  */
 
+    public static void divideWithoutOperator(int n1, int n2) {
 
+        int count = 0;
+
+        while (n1 >= n2) {
+            count++;
+            n1 -= n2;
+        }
+
+        System.out.println( count + " with remainder " + n1);
+
+    }
+
+}
+/* Task 2:
+ Create am method that can divide two numbers without using division operator
+*/
 
