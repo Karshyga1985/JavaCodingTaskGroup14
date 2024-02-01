1ec204de94b21b7e63440ba9957ce8901a570318
Christine <christinessok@gmail.com>
2024-01-31 20:18:38 -0500

    finra task added

--- src/ChristineFirstWeekTask.java
+++ src/ChristineFirstWeekTask.java
@@ -56,6 +56,22 @@ a multiple of 5, print "RA" instead of the number. For numbers which are a
 multiple of both 3 and 5, print "FINRA" instead of the number.
      */
 
+    public static void finra30() {
+
+        for (int i = 1; i < 31; i++) {
+
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print(" FINRA");
+            } else if (i % 3 == 0) {
+                System.out.print(" FIN");
+            } else if (i % 5 == 0) {
+                System.out.print(" RA");
+            } else {
+                System.out.print(" " + i);
+            }
+        }
+    }
+
 
 
 }
