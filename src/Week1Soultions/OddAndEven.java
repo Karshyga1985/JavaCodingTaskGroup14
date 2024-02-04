562c7b751ca00adf3babbbdceb3d9a2885e73d2f
arosta16 <arosta16@gmail.com>
2024-02-04 10:24:56 -0500

    Adri week1 tasks

---
+++ src/Week1Soultions/OddAndEven.java
@@ -0,0 +1,14 @@
+package Week1Soultions;
+
+public class OddAndEven {
+
+    public static void oddOrEven(int num) {
+
+        if (num % 2 == 0) {
+            System.out.println(num + " is even number");
+        } else {
+            System.out.println(num + " is odd number");
+
+        }
+    }
+}
