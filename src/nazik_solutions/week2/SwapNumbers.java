26e48ad906fbbe5b3e8430fe3388d3ef8b07c890
nazikt <nazik.turdakunova@gmail.com>
2024-02-06 14:22:32 -0500

    added week 2 task solution

---
+++ src/nazik_solutions/week2/SwapNumbers.java
@@ -0,0 +1,24 @@
+package nazik_solutions.week2;
+
+public class SwapNumbers{
+        public static void main(String[] args) {
+
+            int a = 100,  // Using addition and subtraction
+                    b = 50;
+
+            a = a + b;  // x becomes 150
+
+            b = a - b;  // y becomes 100
+
+            a = a - b;  // x becomes 50
+
+            System.out.println("a = " + a + "\n" + "b = " + b);
+
+
+        }
+
+}
+ /*
+    Swap two variable values without using a third variable
+     */
+
