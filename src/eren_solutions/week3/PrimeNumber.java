f9a96c57cedbbc34d8cfe9df9312a75a3e348a14
celikerCydeo <celik+celik.eren57@hotmail.com>
2024-02-16 17:38:58 -0500

    EREN's Week3 Tasks solutions created

---
+++ src/eren_solutions/week3/PrimeNumber.java
@@ -0,0 +1,25 @@
+package eren_solutions.week3;
+
+public class PrimeNumber {
+
+
+    public static void main(String[] args) {
+
+/*
+        1. Numbers -- Prime Number
+
+        Write a method that can check if a number is prime or not.
+
+
+*/
+
+
+
+
+
+
+
+
+
+    }
+}
--- src/week3/Task2.java
+++ src/week3/Task2.java
@@ -1,4 +1,15 @@
 package week3;
 
 public class Task2 {
+
+    /*
+
+    2. Numbers -- Reverse Negative Number
+
+    Write a return method that can reverse negative number and return it as int.
+
+
+    */
+
+
 }
