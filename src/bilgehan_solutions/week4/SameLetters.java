5cab096b9d8c64e79e76e3029341439ef91df4bf
sofiabrown91 <bilgehanbulbul91@gmail.com>
2024-02-27 22:42:18 -0500

    week 4 solutions

---
+++ src/bilgehan_solutions/week4/SameLetters.java
@@ -0,0 +1,42 @@
+package bilgehan_solutions.week4;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SameLetters {
+    public static void main(String[] args) {
+        System.out.println(sameLetters("abcdeee", "cebdaeet"));
+        System.out.println(sameLetters("abcdeee", "cebdaee"));
+
+    }
+
+    public static boolean sameLetters(String word1, String word2) {
+
+        if (word1.length() != word2.length()) {
+            return false;
+        }
+
+        List<String> word1ch = new ArrayList<>(Arrays.asList(word1.split("")));
+        List<String> word2ch = new ArrayList<>(Arrays.asList(word2.split("")));
+
+        for (String each1 : word1ch) {
+            for (int j = 0; j < word2ch.size(); j++) {
+                word2ch.removeIf(p -> p.equals(each1));
+            }
+        }
+        if (word2ch.size() == 0) {
+            return true;
+        }
+        return false;
+
+    }
+
+
+}
+/*
+        Write a return method that check if a string is build out of the
+        same letters as another string.
+        Ex: same("abc", "cab"); -> true
+        same("abc", "abb"); -> false:
+     */
