f656b2408d6f592b98e21d4b058fad1fe4625531
Karshyga1985 <kbayatova@gmail.com>
2024-03-05 17:54:17 -0500

    week5 FindUnique task solution

--- src/week5/String_FindUnique.java
+++ src/week5/String_FindUnique.java
@@ -1,12 +1,10 @@
 package week5;
 
 public class String_FindUnique {
-
-
-    /*String -- Find the unique
-Write a return method that can find the unique characters from the String
-Ex: unique("AAABBBCCCDEF") ==> "DEF";
-
+    /*
+    String -- Find the unique
+    Write a return method that can find the unique characters from the String
+    Ex: unique("AAABBBCCCDEF") ==> "DEF";
 */
 
 
