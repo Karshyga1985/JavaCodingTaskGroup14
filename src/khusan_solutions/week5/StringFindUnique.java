167ea0bb4fb60b90d8d3e6da74dca32de73e69a6
khusansamatov <samatov987@gmail.com>
2024-03-06 14:14:32 -0500

    update 5th week task StringFindUnique

--- src/khusan_solutions/week5/StringFindUnique.java
+++ src/khusan_solutions/week5/StringFindUnique.java
@@ -4,36 +4,29 @@ public class StringFindUnique {
     public static void main(String[] args) {
 
         //String unique = "AAABBBCCCDEF";
-        unique("AAABBBCCCDEF");
-
+        System.out.println(unique("AAABBBCCCDEFTW"));
+        System.out.println(unique("AAABBBCCCDQEEFTWAL"));
 
     }
 
-    public static void unique(String str){
-
+    public static String unique(String str){
         String result = "";
 
         for (int i = 0; i < str.length(); i++) {
             int count = 0;
 
-            if( ! (result.contains(""+str.charAt(i)) ) ){
-
-                for (int j = 0; j < str.length(); j++) {
-
-                    if(str.charAt(i) == str.charAt(j)){
-                        count++;
-                    }
-                }
-                result += "" + str.charAt(i)+count;
-
-                if(count == 1){
-                    System.out.print(str.charAt(i));
+            for (int j = 0; j < str.length(); j++) {
+                if(str.charAt(i) == str.charAt(j)){
+                    count++;
                 }
             }
-        }
-        //System.out.println(result);
-    }
 
+            if(count == 1){
+                result += str.charAt(i);
+            }
+        }
+        return result;
+    }
 }
 
 
