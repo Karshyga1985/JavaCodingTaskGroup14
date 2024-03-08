ff903993c98e984922956cfd68439b1e6f50c93a
ChristinesSok <christinessok@gmail.com>
2024-03-08 12:20:13 -0500

    week6 tasks added

---
+++ src/christine_solutions/week6/Array_FindMaximum.java
@@ -0,0 +1,9 @@
+package christine_solutions.week6;
+
+public class Array_FindMaximum {
+}
+
+/*
+Array -- Find Maximum
+Write a method that can find the maximum number from an int Array
+ */
---
+++ src/christine_solutions/week6/SumOfDigits.java
@@ -0,0 +1,9 @@
+package christine_solutions.week6;
+
+public class SumOfDigits {
+}
+
+/*
+String -- sum of digits in a string
+Write a method that can return the sum of the digits in a string
+ */
