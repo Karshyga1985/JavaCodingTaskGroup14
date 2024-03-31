3ec067c0abcfdcb2b06447039cc6e2938546ba20
celikerCydeo <celik+celik.eren57@hotmail.com>
2024-03-31 18:07:52 -0400

    Week-8 Tasks Solutions fixed

--- src/eren_solutions/week8/Array_UniqueIntegersOfSumZero.java
+++ src/eren_solutions/week8/Array_UniqueIntegersOfSumZero.java
@@ -8,8 +8,6 @@ import java.util.Set;
 public class Array_UniqueIntegersOfSumZero {
 
 
-    public class Array_UniqueIntegersSumZero {
-
 /*Array - N unique integers that sum up to 0
 Write a function that given an integer N (1 < N < 100), returns an array
 containing N unique integers that sum up to 0. The function can return any
@@ -18,7 +16,6 @@ For example, given N = 4, the function could return [1,0, -3,2] or [-2,1, -4,5].
 The answer [1, - 1,1,3] would be incorrect (because value 1 occurs twice).
 For N = 3, one of the possible answers is [-1,0,1] (but there are many more
 correct answers).*/
-
 
         public static void main(String[] args) {
             int N = 3;
@@ -60,8 +57,7 @@ correct answers).*/
                 return result;
             }
 
-
         }
 
     }
-}
+
