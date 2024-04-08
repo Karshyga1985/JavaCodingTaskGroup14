804070e2d93139990766b8a23d5b14c862f9fbd6
arosta16 <arosta16@gmail.com>
2024-04-08 19:21:11 -0400

    Adri week 10 solutions update

---
+++ src/Adri_Solutions/week10Solutions/Arraylist_sorting_descending.java
@@ -0,0 +1,47 @@
+package Adri_Solutions.week10Solutions;
+
+import java.util.ArrayList;
+
+public class Arraylist_sorting_descending {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(3);
+        list.add(5);
+        list.add(36);
+        list.add(78);
+        list.add(23);
+        list.add(67);
+        list.add(45);
+        list.add(43);
+        list.add(90);
+
+        System.out.println(sortDesc(list));
+    }
+
+    public static ArrayList<Integer> sortDesc(ArrayList<Integer> list) {
+        for (int i = 0; i < list.size(); i++) {
+
+            for (int j = i + 1; j < list.size(); j++) {
+
+                int TempI = list.get(i);
+                int TempJ = list.get(j);
+
+                if (TempI < TempJ) {
+                    list.set((i), TempJ);
+                    list.set((j), TempI);
+
+                }
+
+            }
+        }
+        return list;
+    }
+
+
+}
+/*
+ArrayList - sorting in descending order
+Write a method that can sort the ArrayList in descending order without
+using the sort method.
+ */
