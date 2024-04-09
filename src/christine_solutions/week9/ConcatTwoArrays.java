89f4833fd80539c68db650a3524a04379975825d
ChristinesSok <christinessok@gmail.com>
2024-04-09 11:19:03 -0400

    added tasks for week 9

---
+++ src/christine_solutions/week9/ConcatTwoArrays.java
@@ -0,0 +1,8 @@
+package christine_solutions.week9;
+
+public class ConcatTwoArrays {
+    /*
+    1) Array - Concat two arrays
+    Write a return method that can concate two arrays
+     */
+}
---
+++ src/christine_solutions/week9/RemoveAhmed.java
@@ -0,0 +1,9 @@
+package christine_solutions.week9;
+
+public class RemoveAhmed {
+    /*
+    2) ArrayList - Remove "Ahmed"
+    Given a list of people' names: "Ahmed", "John", Eric", "Ahmed".....
+    Write a java operation to remove all the names named Ahmed
+     */
+}
