601bade9fae71d5e6daa81cd046bec017a19d4ea
Karshyga1985 <kbayatova@gmail.com>
2024-04-23 08:40:28 -0400

    add week11 tasks desc

---
+++ src/week11/MapFrequencyOfChar.java
@@ -0,0 +1,10 @@
+package week11;
+
+public class MapFrequencyOfChar {
+    /*
+        Map-- Frequency of Characters
+    `Write a method that prints the frequency of each character from a String.
+     Example: input => "hello world"
+     output => {h=1, e=1, l=3, o=2,  =1, w=1, r=1, d=1}
+     */
+}
---
+++ src/week11/MapMinValue.java
@@ -0,0 +1,9 @@
+package week11;
+
+public class MapMinValue {
+    /*
+    Map - Min value
+Write a method that can return the minimum value from a map
+(DO NOT use sort method).
+     */
+}
