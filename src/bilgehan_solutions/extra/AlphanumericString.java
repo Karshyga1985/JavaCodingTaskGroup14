eb0acc351980a5d16a492ada602611a1f4fa0cc5
sofiabrown91 <bilgehanbulbul91@gmail.com>
2024-05-01 17:41:28 -0400

    Bilgehan updated week10

--- src/bilgehan_solutions/extra/AlphanumericString.java
+++ src/bilgehan_solutions/extra/AlphanumericString.java
@@ -1,16 +1,91 @@
 package bilgehan_solutions.extra;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 public class AlphanumericString {
     public static void main(String[] args) {
         String str = "DCER501GCCCA098911";
-        System.out.println(sortString(str));
+        // System.out.println(sortString(str));
+        String result = "";
+        String sorted = "";
+
+        for (int i = 0; i < str.length() - 1; i++) {
+            Character ch = str.charAt(i);
+            Character ch1 = str.charAt(i + 1);
+            if (Character.isLetter(ch)) {
+                if (Character.isLetter(ch1)) {
+                    result += ch + "";
+                } else {
+                    result += ch + ",";
+                }
+            }
+            if (Character.isDigit(ch)) {
+                if (Character.isDigit(ch1)) {
+                    result += ch + "";
+                } else {
+                    result += ch + ",";
+                }
+            }
+        }
+
+        System.out.println(result);
+        String[] arr1 = result.split(",");
+        ArrayList<String> list = new ArrayList<>(Arrays.asList(arr1));
+        ArrayList<String> list2 = new ArrayList<>();
+
+
+        for (String each : list) {
+            char[] arr2 = each.toCharArray();
+            String elm = "";
+            for (int i = 0; i < arr2.length - 1; i++) {
+                for (int j = 0; j < arr2.length - 1; j++) {
+                    if (arr2[j] > arr2[j + 1]) {
+                        char temp = arr2[j + 1];
+                        arr2[j + 1] = arr2[j];
+                        arr2[j] = temp;
+                    }
+                }
+
+            }
+            System.out.println(Arrays.toString(arr2));
+
+            for (char each1 : arr2) {
+                elm += "" + each1;
+            }
+            sorted += elm;
+            //  list2.add(elm);
+        }
+//        System.out.println(list2);
+//        for (String s : list2) {
+//            sorted += s;
+//        }
+        System.out.println("sorted = " + sorted);
     }
+
 
     public static String sortString(String str) {
         String result = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            Character ch = str.charAt(i);
+            Character ch1 = str.charAt(i + 1);
+            if (Character.isLetter(ch)) {
+                if (Character.isLetter(ch1)) {
+                    result += ch + "";
+                } else {
+                    result += ch + ",";
+                }
+            }
+            if (Character.isDigit(ch)) {
+                if (Character.isDigit(ch1)) {
+                    result += ch + "";
+                } else {
+                    result += ch + ",";
+                }
+            }
+        }
 
 
         return result;
--- src/bilgehan_solutions/extra/Permutation.java
+++ src/bilgehan_solutions/extra/Permutation.java
@@ -1,6 +1,27 @@
 package bilgehan_solutions.extra;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Permutation {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4};
+        ArrayList<Map<Integer, Integer>> list = new ArrayList<>();
+        ArrayList<Map<Integer, Integer>> list2 = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            Map<Integer, Integer> maps = new LinkedHashMap<>();
+            for (int j = 0,k=arr.length-1; j < arr.length; j++,k--) {
+              if(arr[k] !=arr[i]){
+                  maps.put(arr[k] , arr[i]);
+              }
+            } list.add(maps);
+        }
+        System.out.println(list);
+
+    }
 }
 //    Given an array of 3 characters print all permutation combinations from the given
 //        characters
